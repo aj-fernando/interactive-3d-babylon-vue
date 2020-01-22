dff7c5b9f13ba4d40707892dbc77d5874f7b33f5
AJ <aj.fernando@holoplot.com>
2020-01-22 18:42:29 +0100

    Load a 3D venue, Add Hemispheric light & Directional light to Entity

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,23 +2,35 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <Scene v-model="myScene">
+      <Property name="clearColor" color="#FFF"></Property>
       <Camera type="arcRotate" :alpha="20" :beta="-15" :radius="70" :position="[100, 100, 100]"></Camera>
-      <HemisphericLight diffuse="#005"></HemisphericLight>
+      <HemisphericLight diffuse="#000"></HemisphericLight>
+
+      <Asset :src="publicPath+'Stadium.obj'"></Asset>
+
       <Entity v-model="myEntity">
-        <Box v-model="myBox"></Box>
+        <DirectionalLight specular="#0F0" diffuse="F00" :direction="[0,0,1]"></DirectionalLight>
+        <Box v-model="myBox" :position="[0,30,-20]" :scaling="[5,5,5]"></Box>
       </Entity>
     </Scene>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import vb from 'vue-babylonjs';
+import { Component, Prop, Vue } from "vue-property-decorator";
+import vb from "vue-babylonjs";
 Vue.use(vb);
 
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+
+  private publicPath: string;
+
+  constructor() {
+    super();
+    this.publicPath = `${process.env.BASE_URL}`;
+  }
 }
 </script>
 
