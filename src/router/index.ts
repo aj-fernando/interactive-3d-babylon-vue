10f828254db7e4f07abb4e1ff3f3187edf393d93
AJ <aj.fernando@holoplot.com>
2020-01-24 18:04:15 +0100

    Add ClickablesContainer and  routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/ObservablesContainer.vue')
+  },
+  {
+    path: '/clickables',
+    name: 'clickables',
+    component: () => import(/* webpackChunkName: "about" */ '../views/ClickablesContainer.vue')
   }
 ]
 
---
+++ src/views/ClickablesContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="clickables">
+    <h1>Load clickable 3D meshes and generate events</h1>
+    <Clickables></Clickables>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Clickables from "@/components/Clickables.vue";
+
+@Component({
+  components: {
+    Clickables
+  }
+})
+export default class ClickablesContainer extends Vue {}
+</script>
