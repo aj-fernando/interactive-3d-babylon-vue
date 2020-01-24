3148c7f11e2a2f35bdccb8bca2abe3cdcf9f1808
AJ Fernando <aj.fernando@holoplot.com>
2020-01-24 02:05:35 +0100

    Try vue @click on v-if for a button & a  Babylon object
    works on button not on the 3d model on canvas

---
+++ src/components/Playground.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1> Hello Playground </h1>
+      <button @click="test()" v-if="this.counter >= 2" > Click Me </button>
+      <Scene>
+        <Box v-on:click="test()"></Box>
+      </Scene>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import vb from "vue-babylonjs";
+Vue.use(vb);
+
+@Component
+export default class Playground extends Vue {
+
+    private counter: number
+
+    constructor() {
+      super();
+      this.counter = 2;
+    }
+    test() {
+        console.log("Hello");
+    }
+
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Playground></Playground>
   </div>
 </template>
+
+
+<script lang="ts">
+
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Playground from "@/components/Playground.vue"
+
+@Component ({
+  components: {
+    Playground,
+  }
+})
+export default class About extends Vue {
+}
+</script>
